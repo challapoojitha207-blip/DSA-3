2409fe711f5c5f7d16670b6294ce3c1c410da5fa
challapoojitha207-blip <challapoojitha207@gmail.com>
2026-08-16 16:05:57 +0530

    Delete PRACTICAL/PRACTICAL-3 directory

--- PRACTICAL/PRACTICAL-3/main (3).py
+++
@@ -1,9 +0,0 @@
-with open("articles.txt", "r") as file:
-    text = file.read()
-
-query = input("Enter a keyword: ")
-
-if query.lower() in text.lower():
-    print("Keyword found.")
-else:
-    print("Keyword not found.")
